e5d44f5fa404c970994a2c3301e07e3a3446274a
Florian Menne <flo90dev@gmail.com>
2014-02-19 16:30:25 +0100

    just changed year in boilerplate;

--- rpi_hx2262app.c
+++ rpi_hx2262app.c
@@ -1,6 +1,6 @@
 /*
    
-   Copyright (C) 2013 Florian Menne (florianmenne@t-online.de)
+   Copyright (C) 2014 Florian Menne (florianmenne@t-online.de)
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
